bcf7415407dd93e156d33c495092f654c9b895d5
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-04-14 01:52:36 +0530

    Resolving conflicts

--- lib/screens/student_homepage.dart
+++ lib/screens/student_homepage.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:devopia_overload_oblivion/Helper/helper_function.dart';
 import 'package:devopia_overload_oblivion/resources/auth_methods.dart';
-import 'package:devopia_overload_oblivion/screens/teacher_homepage.dart';
+import 'package:devopia_overload_oblivion/screens/assignments.dart';
+import 'package:devopia_overload_oblivion/screens/short_answer_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:devopia_overload_oblivion/resources/database.dart';
@@ -10,8 +11,6 @@ import 'package:devopia_overload_oblivion/resources/database.dart';
 import 'package:devopia_overload_oblivion/screens/quiz_play.dart';
 // import 'package:devopia_overload_oblivion/screens/short_answer_screen.dart';
 import 'package:devopia_overload_oblivion/screens/user_type_selec.dart';
-
-
 
 class StudentHomepage extends StatefulWidget {
   const StudentHomepage({super.key});
@@ -63,7 +62,8 @@ class _StudentHomepageState extends State<StudentHomepage> {
       ),
     );
   }
- String email = "";
+
+  String email = "";
   String name = "";
   @override
   void initState() {
@@ -74,18 +74,18 @@ class _StudentHomepageState extends State<StudentHomepage> {
     super.initState();
     getUserDetails();
   }
-  void getUserDetails() async{
-    await HelperFunction.getUserEmail().then((value){
+
+  void getUserDetails() async {
+    await HelperFunction.getUserEmail().then((value) {
       setState(() {
         email = value!;
       });
     });
-    await HelperFunction.getUserName().then((value){
+    await HelperFunction.getUserName().then((value) {
       setState(() {
         name = value!;
       });
     });
-    
   }
 
   @override
@@ -116,34 +116,7 @@ class _StudentHomepageState extends State<StudentHomepage> {
         backgroundColor: Colors.transparent,
       ),
       drawer: Drawer(
-      
         child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromRGBO(99, 151, 255, 1),
-                    Color.fromRGBO(31, 68, 255, 1)
-                  ],
-                  stops: [0.1, 1],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              title: const Text('Sign Out'),
-              onTap: () async{
-                 await auth.signout();
           padding: const EdgeInsets.symmetric(vertical: 50),
           children: <Widget>[
             const Icon(
@@ -159,77 +132,91 @@ class _StudentHomepageState extends State<StudentHomepage> {
             ),
             const SizedBox(height: 30),
             const Divider(
-            height: 2,
-          ),
-          
-         
-          ListTile(
-            onTap: () async {
-              showDialog(
-                  barrierDismissible: false,
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text("Logout"),
-                      content: const Text("Are you sure you want to logout?"),
-                      actions: [
-                        IconButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: const Icon(
-                            Icons.cancel,
-                            color: Colors.red,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () async {
-                            await auth.signout();
-                            Navigator.of(context).pushAndRemoveUntil(
-                                MaterialPageRoute(
-                                    builder: (context) => const UserTypeSelectionPage()),
-                                (route) => false);
-                // Navigator.pushReplacement(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) => UserTypeSelectionPage(),
-                //   ),
-                // );
-              },
+              height: 2,
             ),
+            ListTile(
+              onTap: () async {
+                showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text("Logout"),
+                        content: const Text("Are you sure you want to logout?"),
+                        actions: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(
+                              Icons.cancel,
+                              color: Colors.red,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              await auth.signout();
+                              Navigator.of(context).pushAndRemoveUntil(
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const UserTypeSelectionPage()),
+                                  (route) => false);
+                            },
+                            icon: const Icon(
+                              Icons.done,
+                              color: Colors.green,
+                            ),
+                          ),
+                        ],
+                      );
+                    });
+              },
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text(
+                "Logout",
+                style: TextStyle(color: Colors.black),
+              ),
+            )
           ],
         ),
       ),
       body: quizList(),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(), // Create a notch for FAB
-        child: Row(
-          children: [
-            // Other bottom nav items if any
-            Spacer(),
-            Container(
-              width: 48,
-              height: 48,
-              child: IconButton(
-                onPressed: () => setState(() => isCreateMode = true),
-                icon: Icon(Icons.create, size: 39),
-                color: isCreateMode
-                    ? Colors.blue
-                    : Colors.grey, // Highlight active button
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+        height: 80,
+        padding: EdgeInsets.all(2.0),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          child: Row(
+            children: [
+              // Other bottom nav items if any
+              Spacer(),
+              Container(
+                width: 48,
+                height: 48,
+                child: IconButton(
+                  onPressed: () => setState(() => isCreateMode = true),
+                  icon: Icon(Icons.create, size: 39),
+                  color: isCreateMode
+                      ? Colors.blue
+                      : Colors.grey, // Highlight active button
+                ),
               ),
-            ),
-            SizedBox(width: 96),
-            Container(
-              width: 48,
-              height: 48,
-              child: IconButton(
-                onPressed: () => setState(() => isCreateMode = false),
-                icon: Icon(Icons.upload, size: 39),
-                color: !isCreateMode ? Colors.blue : Colors.grey,
+              SizedBox(width: 96),
+              Container(
+                width: 48,
+                height: 48,
+                child: IconButton(
+                  onPressed: () => setState(() => isCreateMode = false),
+                  icon: Icon(Icons.upload, size: 39),
+                  color: !isCreateMode ? Colors.blue : Colors.grey,
+                ),
               ),
-            ),
-            Spacer(),
-          ],
+              Spacer(),
+            ],
+          ),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -239,41 +226,16 @@ class _StudentHomepageState extends State<StudentHomepage> {
             Navigator.push(
               context,
               MaterialPageRoute(
-                // builder: (context) => ShortAnswerScreen(),
-                builder: (context) => Scaffold(),
+                builder: (context) => ShortAnswerScreen(),
+                // builder: (context) => Scaffold(),
               ),
             );
           } else {
             Navigator.push(
               context,
               MaterialPageRoute(
-                // builder: (context) => Assignments(),
-                builder: (context) => Scaffold(),
-              ),
-            );
-          }
-        },
-        child: Icon(isCreateMode
-            ? Icons.short_text_sharp
-            : Icons.assignment), // Change icon based on mode
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (isCreateMode) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                // builder: (context) => ShortAnswerScreen(),
-                builder: (context) => Scaffold(),
-              ),
-            );
-          } else {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                // builder: (context) => Assignments(),
-                builder: (context) => Scaffold(),
+                builder: (context) => Assignments(),
+                // builder: (context) => Scaffold(),
               ),
             );
           }
