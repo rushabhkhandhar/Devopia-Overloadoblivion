139b42c8415224b5eba7a1ef85ae8c3c285444df
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-04-13 20:45:06 +0530

    Quiz API integration for personalized quiz

--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -1,5 +1,4 @@
-class QuestionModel{
-
+class QuestionModel {
   String question;
   String option1;
   String option2;
@@ -7,14 +6,27 @@ class QuestionModel{
   String option4;
   String correctOption;
   bool answered;
+  bool isCorrect;
 
-  QuestionModel({
-    required this.question,
-    required this.option1,
-    required this.option2,
-    required this.option3,
-    required this.option4,
-    required this.correctOption,
-    required this.answered
-  });
+  QuestionModel(
+      {required this.question,
+      required this.option1,
+      required this.option2,
+      required this.option3,
+      required this.option4,
+      required this.correctOption,
+      required this.answered,
+      this.isCorrect = false});
+  Map<String, dynamic> toJson() {
+    return {
+      'question': question,
+      'option1': option1,
+      'option2': option2,
+      'option3': option3,
+      'option4': option4,
+      'correctOption': correctOption,
+      'answered': answered,
+      'isCorrect': isCorrect
+    };
+  }
 }
--- lib/screens/chat_help2.dart
+++ lib/screens/chat_help2.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:devopia_overload_oblivion/screens/quiz_play.dart';
 import 'package:flutter/material.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:http/http.dart' as http;
@@ -6,7 +7,8 @@ import 'package:devopia_overload_oblivion/screens/student_homepage.dart';
 import 'secrets.dart';
 
 class chatHelp2 extends StatefulWidget {
-  const chatHelp2({super.key});
+  final List<Map<String, dynamic>> questions;
+  const chatHelp2({super.key, required this.questions});
 
   @override
   State<chatHelp2> createState() => _chatHelp2State();
@@ -55,6 +57,75 @@ class _chatHelp2State extends State<chatHelp2> {
     }).catchError((e) {});
 
     typing.remove(bot);
+  }
+
+  giveTemplate() async {
+    await http
+        .post(Uri.parse(ourUrl),
+            headers: header,
+            body: jsonEncode({
+              "contents": [
+                {
+                  "parts": [
+                    {
+                      "text":
+                          "Give me the resources for the questions that I was weak at in the last test the data is ${questions.toString()} analyze and determine where I need to improve provide links to resources"
+                    }
+                  ]
+                }
+              ]
+            }))
+        .then((value) {
+      if (value.statusCode == 200) {
+        var result = jsonDecode(value.body);
+        print(result['candidates'][0]['content']['parts'][0]['text']);
+
+        ChatMessage m1 = ChatMessage(
+            text: result['candidates'][0]['content']['parts'][0]['text'],
+            user: bot,
+            createdAt: DateTime.now());
+        allMessages.insert(0, m1);
+        setState(() {});
+      } else {
+        print("error occured");
+      }
+    }).catchError((e) {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    giveTemplate();
+
+    // Add template prompt as the first question
+    // ChatMessage templatePrompt = ChatMessage(
+    //   text: "How can I assist you today?",
+    //   user: bot,
+    //   createdAt: DateTime.now(),
+    // );
+    // allMessages.add(templatePrompt);
+    // List<ChatMessage> prompts = [
+    //   ChatMessage(
+    //     text: "Suggest resources for the topic that I will ask",
+    //     user: myself, // Prompt from user
+    //     createdAt: DateTime.now(),
+    //   ),
+    //   // ChatMessage(
+    //   //   text: "Explain the concept of photosynthesis.",
+    //   //   user: myself, // Prompt from user
+    //   //   createdAt: DateTime.now(),
+    //   // ),
+    //   // Add more prompts as needed
+    // ];
+
+    // allMessages.addAll(prompts);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    questions.clear();
   }
 
   @override
